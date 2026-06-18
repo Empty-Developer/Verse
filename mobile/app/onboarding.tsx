eb90f375aa37f191ad64e3915b09e32a981ba58c
Empty <emptypageworking@gmail.com>
2026-06-18 15:06:11 +0700

    Feat: appnd pagination
    pagination for slider : not ready yet

--- mobile/app/onboarding.tsx
+++ mobile/app/onboarding.tsx
@@ -1,11 +1,15 @@
-import { FlatList, StyleSheet, Text, View} from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View, ViewToken } from "react-native";
+import React from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
-import Animated, { useAnimatedRef, useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
-import RenderItem from '@/app/components/RenderItem';
-import slider, { OnboardingData } from '@/service/options/OnboardingService'
-
+import Animated, {
+  useAnimatedRef,
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
+import RenderItem from "@/app/components/RenderItem";
+import slider, { OnboardingData } from "@/service/options/OnboardingService";
+import Pagination from "@/service/pagination/pagination";
 
 export default function Onboarding() {
   const finishOnboarding = async () => {
@@ -19,36 +23,76 @@ export default function Onboarding() {
     create a sharedValue named x
     for the data scroll
   */
- const x = useSharedValue(0)
+  const x = useSharedValue(0);
 
- const onScroll = useAnimatedScrollHandler({
-  onScroll: event => {
-    x.value = event.contentOffset.x
-  }
- })
+  const flatListIndex = useSharedValue(0);
+
+  const onViewableItemsChanged = ({
+    viewableItems,
+  }: {
+    viewableItems: ViewToken[];
+  }) => {
+    if (viewableItems[0].index !== null) {
+      flatListIndex.value = viewableItems[0].index;
+    }
+  };
+
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      x.value = event.contentOffset.x;
+    },
+  });
 
   return (
     <View style={styles.container}>
       <Animated.FlatList
         data={slider}
-        renderItem={({item, index}) => {
-          return <RenderItem item={item} index={index} x={x}/>
+        renderItem={({ item, index }) => {
+          return <RenderItem item={item} index={index} x={x} />;
         }}
-        keyExtractor={item => item.id}
+        // keyExtractor={item => item.id}
         onScroll={onScroll}
         scrollEventThrottle={16}
         horizontal={true}
         bounces={false}
         pagingEnabled={true}
         showsHorizontalScrollIndicator={false}
-
+        /*
+          onViewableitemsChanged called
+          when the viewability of rows changes
+          as defined by the viewabilityConfig prop
+        */
+        onViewableItemsChanged={onViewableItemsChanged}
+        viewabilityConfig={{
+          /*
+            minimumViewTime is minimum amount
+            of time (in milliseconds) that
+            an item must be physically viewable
+            before the viewability callback will
+            be fired
+          */
+          minimumViewTime: 300,
+          viewAreaCoveragePercentThreshold: 10,
+        }}
       />
+      <View style={styles.bottomContainer}>
+        {/* pass data and x to pagination */}
+        <Pagination data={slider} x={x}/>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-})
+  bottomContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0,
+    marginHorizontal: 30,
+    paddingVertical: 30,
+  }
+});
---
+++ mobile/service/pagination/Pagination.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { OnboardingData } from '../options/OnboardingService'
+import { SharedValue } from 'react-native-reanimated';
+
+type Props = {
+  slider: OnboardingData[];
+  x: SharedValue<number>;
+}
+
+const Pagination = ({slider, x}: Props) => {
+  return (
+    <View style={styles.paginationContainer}>
+      {slider.map((_, index) => {
+        return <Dot key={index} index={index} x={x}/>
+      })}
+    </View>
+  )
+}
+
+export default Pagination
+
+const styles = StyleSheet.create({
+  paginationContainer: {
+    flexDirection: 'row',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
