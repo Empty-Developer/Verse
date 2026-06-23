8a9533e4bbddaf2c59ab223b1e171809cb2341fa
Empty <emptypageworking@gmail.com>
2026-06-23 13:42:17 +0700

    Feat: Type user for save db

---
+++ stores/useAuthStore.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand'
+
+type AuthStore = {
+
+}
+
+/**
+ * @description this function - create
+ * a new type data user useAuthStore(name example)
+ */
+export const useAuthStore = create<AuthStore>()(
+
+)
