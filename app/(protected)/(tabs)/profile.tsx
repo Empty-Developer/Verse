6ee89e95f9ee60ab72a6ecd0de8ed3a90e219839
Empty <emptypageworking@gmail.com>
2026-06-28 13:04:37 +0700

    Feat: added upload phote and take photo from supabase[user]
    a system was created that allows users to access the database, upload photos, and change their profile picture.

--- app/(protected)/(tabs)/profile.tsx
+++ app/(protected)/(tabs)/profile.tsx
@@ -1,7 +1,9 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as ImagePicker from "expo-image-picker";
+import { uploadProfileImage } from "@/lib/storage";
+import { supabase } from "@/lib/supabase";
 
 export default function SettingsScreen() {
   const [profileImage, setProfileImage] = useState<string | null>(null);
@@ -27,10 +29,73 @@ export default function SettingsScreen() {
       quality: 0.8,
     });
 
-    if (!result.canceled && result.assets[0]) {
-      setProfileImage(result.assets[0].uri);
+    if (result.canceled || !result.assets[0]) return;
+
+    const imageUri = result.assets[0].uri;
+
+    setProfileImage(imageUri);
+
+    try {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (!user) return;
+
+      // upload in storage
+      const publicUrl = await uploadProfileImage(user.id, imageUri);
+
+      await supabase
+        .from("profiles")
+        .update({
+          avatar_url: publicUrl,
+        })
+        .eq("id", user.id);
+
+      console.log("Avatar uploaded:", publicUrl);
+    } catch (error) {
+      console.error(error);
     }
   };
+
+  /**
+   * @description this function sends a
+   * request to the user table and
+   * retrieves only the avatar
+   * photo from there
+   * @returns {string}
+   */
+  const loadProfileImage = async () => {
+    try {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (!user) return;
+
+      const { data, error } = await supabase
+        /*
+          make a query to
+          the table
+        */
+        .from("profiles")
+        .select("avatar_url")
+        .eq("id", user.id)
+        .single();
+
+      if (error) throw error;
+
+      if (data?.avatar_url) {
+        setProfileImage(data.avatar_url);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    loadProfileImage();
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -39,7 +104,7 @@ export default function SettingsScreen() {
         <TouchableOpacity style={styles.imageContainer} onPress={pickImage}>
           {profileImage ? (
             <Image
-              source={{uri: profileImage}} // take image from picker-expo
+              source={{ uri: profileImage }} // take image from picker-expo
               style={styles.profileImage}
             />
           ) : (
@@ -48,13 +113,13 @@ export default function SettingsScreen() {
                 style={styles.image}
                 source={require("@/assets/images/user.png")}
               />
-          </View>)}
+            </View>
+          )}
           <View style={styles.editBadge}>
             <Text style={styles.editText}>Edit</Text>
           </View>
         </TouchableOpacity>
       </View>
-
     </SafeAreaView>
   );
 }
@@ -104,5 +169,5 @@ const styles = StyleSheet.create({
     width: 160,
     height: 160,
     borderRadius: 100,
-  }
+  },
 });
